58557669378f46105e8ca18b4cef1f124c647e8e
Md <md1040582@gmail.com>
2023-12-22 02:00:29 +0600

    watched list added

---
+++ client/src/components/watched/Watched.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import MovieTitle from '../movies/MovieTitle'
+import MovieCard from '../movies/MovieCard'
+import { useQuery } from 'react-query';
+import Loader from '../authentication/Loader';
+const fetchData = async () => {
+  const email=localStorage.getItem("e")
+  const response = await fetch(`http://localhost:8081/movie/all-watched-movies/${email}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return response.json();
+};
+const Watched = () => {
+  const { data, error, isLoading } = useQuery('myWatched', fetchData);
+  return (
+    <div style={{backgroundImage:"linear-gradient(to right, #06111A, rgba(0, 0, 0, 1))"}}>
+      <div className='ml-3'>
+      <MovieTitle title1={"YOUR"} title2={"WATCH-LIST"}/>
+      </div>
+      {
+        isLoading?
+        <div className='flex items-center justify-center h-full'>
+          <Loader size={35}/>
+        </div>:
+          <MovieCard data={data?.watchedMovies}/>
+
+      }
+    </div>
+  )
+}
+
+export default Watched
